b6c3292c14117397d79024a09e87af31f5256f56
firstblood1985 <limin.hust@foxmail.com>
2018-10-27 08:59:40 +0800

    add print new line

--- test_xml.pl
+++ test_xml.pl
@@ -11,8 +11,9 @@ my $doc = $parser->parse_file('sample_xml_data.xml');
 my @nodes = $doc->getElementsByTagName('AMPrice');
 open my $fh, ">:encoding(UTF-8)", "out.csv" or die "out.csv: $!";
 my $csv = Text::CSV->new ({ binary => 1 });
-my @header = ('PriceId','PriceName','PriceSpec','PriceUnit','PriceNumber','PriceShowDataTime');
+my @header = ('PriceId','PriceName','PriceSpec','PriceUnit','PriceNumber',"PriceShowDataTime");
 $csv->print($fh,\@header);
+
 print $fh "\n";
 
 foreach my $node(@nodes)
