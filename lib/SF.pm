888d4d6723a54879da5f8579d5bd5decca3a6854
firstblood1985 <limin.hust@foxmail.com>
2018-11-18 21:34:03 +0800

    SF first release

--- lib/SF.pm
+++ lib/SF.pm
@@ -5,6 +5,9 @@ use Text::CSV;
 use XML::LibXML;
 use Config::Simple;
 use Data::Dumper;
+use Date::Simple;
+use Date::Range;
+use String::Util 'trim';
 use utf8;
 
 sub new{
@@ -22,7 +25,19 @@ sub process{
     my $ds_name = $ds->{name};
     $self->{handler}->{$ds_name}->($ds);
 }
-sub get_conf{
+sub in_time_range{
+    my ($start_date,$date_from_file,$time_range) = @_;
+    my $start = new Date::Simple($start_date);
+    my $end = $start+$time_range;
+    my $date = new Date::Simple($date_from_file);
+
+    my $range = new Date::Range($start,$end);
+    if($range->includes($date))
+    {
+        return 1;
+    }else{
+        return 0;
+    }
 
 }
 
@@ -46,11 +61,42 @@ sub process_SF{
     my $csv = Text::CSV->new ( { binary => 1,sep_char => "|"} )  # should set binary attribute.
                     or die "Cannot use CSV: ".Text::CSV->error_diag ();
 
+    my $print_csv = Text::CSV->new ( { binary => 1} )  # should set binary attribute.
+                    or die "Cannot use CSV: ".Text::CSV->error_diag ();
+
+    my @header_to_print;
+    push(@header_to_print,$ds->{header}->{2});
+    push(@header_to_print,$ds->{header}->{1});
+
+    foreach(@$header)
+    {
+    push(@header_to_print,$ds->{header}->{$_});
+    }
+
+    $print_csv->print($output_fh,\@header_to_print);
+    print $output_fh "\n";
+
     while ( my $row = $csv->getline( $input_fh) ) {
         #skip row 1 and 2
-        if(1!= $. && 2!= $.)
+        if(1!= $. && 2!= $. && scalar(@$row) > 1)
         {
-            print "@$row";
+            my @results;
+            my $sf_code_from_file = trim($row->[1]);
+            if($sf_code_from_file eq $sf_code)
+            {
+                my $date_from_file = trim($row->[0]);
+                if(in_time_range($start_date,$date_from_file,$time_range))
+                {
+                    push (@results,$sf_code);
+                    push (@results,$date_from_file);
+                    foreach(@$header)
+                    {
+                        push @results,trim($row->[$_-1]);
+                    }
+           $print_csv->print($output_fh,\@results);
+           print $output_fh "\n";
+                }
+            }
         }
       }
 }
